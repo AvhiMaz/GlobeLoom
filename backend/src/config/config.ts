35e5f7f2df9b52fdb69ba529535220ec4683dfc7
AvhiMaz <avhimazumder5@outlook.com>
2024-08-20 22:53:33 +0530

    update config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -10,7 +10,9 @@ const _config = {
   geminiApi: process.env.GEMINI_API_KEY,
   rapidapiKey: process.env.RAPIDAPI_KEY,
   rapidapiHost: process.env.RAPIDAPI_HOST,
-  rapidapiurl: process.env.RAPIDAPI_URL,
+  rapidapiHost2: process.env.RAPIDAPI_HOST2,
+  bookingdotcomapiurl: process.env.BOOKING_DOT_COM_RAPIDAPI_URL,
+  geoCodeapiUrl: process.env.GEOCODE_RAPIDAPI_URL
 };
 
 export const config = Object.freeze(_config);

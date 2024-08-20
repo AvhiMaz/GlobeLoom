a0b1098a4cf84869a2aab431c802ea59d6395660
AvhiMaz <avhimazumder5@outlook.com>
2024-08-20 21:49:40 +0530

    update config to access envs

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -8,6 +8,9 @@ const _config = {
   nodeEnv: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
   geminiApi: process.env.GEMINI_API_KEY,
+  rapidapiKey: process.env.RAPIDAPI_KEY,
+  rapidapiHost: process.env.RAPIDAPI_HOST,
+  rapidapiurl: process.env.RAPIDAPI_URL,
 };
 
 export const config = Object.freeze(_config);

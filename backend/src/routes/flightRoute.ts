c6876e7f6bb5c02cc0fc3b9ca2b782a79444efc5
AvhiMaz <avhimazumder5@outlook.com>
2024-08-23 15:35:29 +0530

    update local repo

--- backend/src/routes/flightRoute.ts
+++ backend/src/routes/flightRoute.ts
@@ -3,7 +3,7 @@ import flightControllers from "../controllers/flightControllers";
 
 const flightRouter = express.Router();
 
-// POST route to search for flights
 flightRouter.post("/flights/search", flightControllers.searchFlights);
 
 export default flightRouter;
+

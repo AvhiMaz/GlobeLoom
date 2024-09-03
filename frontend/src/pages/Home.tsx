1fab033c196108d44abbd15605ae6bb046faa785
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-09-03 19:26:39 +0530

    merged main

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 // import LandingPage from "@/components/Landing";
 import Navbar from "@/components/Navbar";
+import { Carousel } from "@/components/ui/carousel";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Carousel />
+      <Carousel />
       {/* <LandingPage /> */}
     </div>
   );
